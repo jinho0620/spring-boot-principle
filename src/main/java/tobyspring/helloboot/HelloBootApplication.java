487aac8f82559fdc8bfa347f364142c92f35f734
jinho0620 <jinhojo0620@gmail.com>
2025-01-26 23:27:37 +0900

    [Feat] Add servlet with tomcat servlet container to handle http request and response

---
+++ src/main/java/tobyspring/helloboot/HelloBootApplication.java
@@ -0,0 +1,54 @@
+package tobyspring.helloboot;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.io.IOException;
+
+
+@SpringBootApplication
+public class HelloBootApplication {
+
+	public static void main(String[] args) {
+//		TomcatServletWebServerFactory 자체가 tomcat servlet web server는 아니다.
+//		tomcat servlet web server를 만드는 복잡한 생성과정과 설정을 하고, 생성 요청을 하면 생성을 해준다.
+//		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+//		아래와 같이 Jetty 로 web servlet container 를 갈아낄 수도 있다. 이를 위해 받는 type을 interface로 바꿔준다.
+//		ServletWebServerFactory serverFactory = new JettyServletWebServerFactory();
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+
+
+
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					String name = req.getParameter("name");
+
+					resp.setStatus(HttpStatus.OK.value());
+					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+					resp.getWriter().println("Hello " + name);
+				}
+			}).addMapping("/hello");
+        });
+		webServer.start(); // localhost:8080 에 접근하면 404를 던진다. -> tomcat 이 잘 떴음을 확인할 수 있다.
+
+
+	}
+
+}
---
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -0,0 +1,12 @@
+package tobyspring.helloboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello") // Can take query parameter like /hello?name=Jinho
+    public String hello(String name) {
+        return "Hello " + name;
+    }
+}

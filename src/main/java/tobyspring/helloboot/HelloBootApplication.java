a8525dab499d740653721a86cad98a20763c11dd
jinho0620 <jinhojo0620@gmail.com>
2025-01-27 18:10:17 +0900

    [Feat] register beans using @Configuration and @Bean with factory method

--- src/main/java/tobyspring/helloboot/HelloBootApplication.java
+++ src/main/java/tobyspring/helloboot/HelloBootApplication.java
@@ -14,22 +14,35 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import java.io.IOException;
 
 
-@SpringBootApplication
+@Configuration // 이 class는 구성 정보를 가진 class다. -> spring container가 이 안에 @bean이 붙은 factory method가 있겠구나라고 판단.
 public class HelloBootApplication {
+	@Bean // spring container가 사용하는 bean object를 생성하는 factory method라는 의미
+	public HelloController helloController(HelloService helloService) {
+		return new HelloController(helloService);
+	}
 
+	@Bean
+	public HelloService helloService() {
+		return new SimpleHelloService();
+	}
 	public static void main(String[] args) {
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+		// annotation이 붙은 java code를 읽어서 구성 정보를 읽어오는 spring container
+		// @Configuration이 붙은 class를 가장 먼저 등록
+		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
 			@Override
 			protected void onRefresh() { // refresh() method가 실행되는 시점에 hook으로 진행되게 된다.
 				super.onRefresh();
@@ -43,9 +56,8 @@ public class HelloBootApplication {
 				webServer.start(); // localhost:8080 에 접근하면 404를 던진다. -> tomcat 이 잘 떴음을 확인할 수 있다.
 
 			}
-		}; // spring container
-		applicationContext.registerBean(HelloController.class);
-		applicationContext.registerBean(SimpleHelloService.class);
+		};
+		applicationContext.register(HelloBootApplication.class); // 여기서 부터 읽기 시작해라
 		applicationContext.refresh(); // create bean object when the spring container starts up
 
 

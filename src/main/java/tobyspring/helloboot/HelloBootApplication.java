065a1ffe3197f122c9367e9faa93493716d584bd
jinho0620 <jinhojo0620@gmail.com>
2025-01-27 18:59:11 +0900

    [Feat] register servlet factory and dispatcherServlet in bean

--- src/main/java/tobyspring/helloboot/HelloBootApplication.java
+++ src/main/java/tobyspring/helloboot/HelloBootApplication.java
@@ -32,6 +32,16 @@ import java.io.IOException;
 @Configuration // 이 class는 구성 정보를 가진 class다. -> spring container가 이 안에 @bean이 붙은 factory method가 있겠구나라고 판단.
 @ComponentScan // 이 annotation이 붙은 package부터 하위 package들의 @Component가 붙은 class를 전부 뒤져서 bean으로 등록
 public class HelloBootApplication {
+	@Bean
+	public ServletWebServerFactory servletWebServerFactory() {
+		return new TomcatServletWebServerFactory();
+	}
+
+	@Bean
+	public DispatcherServlet dispatcherServlet() {
+		return new DispatcherServlet();
+	}
+
 	public static void main(String[] args) {
 		// annotation이 붙은 java code를 읽어서 구성 정보를 읽어오는 spring container
 		// @Configuration이 붙은 class를 가장 먼저 등록
@@ -40,11 +50,13 @@ public class HelloBootApplication {
 			protected void onRefresh() { // refresh() method가 실행되는 시점에 hook으로 진행되게 된다.
 				super.onRefresh();
 
-				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+				DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+				// dispatcher servlet은 호출할 controller가 필요하기 때문에 spring container가 필요하다.
+				// -> setter로 spring container를 주입
 				WebServer webServer = serverFactory.getWebServer(servletContext -> {
-					servletContext.addServlet("dispatcherServlet",
-							new DispatcherServlet(this)
-					).addMapping("/*");
+					servletContext.addServlet("dispatcherServlet", dispatcherServlet)
+							.addMapping("/*");
 				});
 				webServer.start(); // localhost:8080 에 접근하면 404를 던진다. -> tomcat 이 잘 떴음을 확인할 수 있다.
 
--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,22 +1,34 @@
 package tobyspring.helloboot;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.Aware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Objects;
 @RestController // @ResponseBody를 포함 -> 하위에 있는 모든 method의 return 값을 body 에 넣어서 return 한다.
-@RequestMapping("/hello")
+//@RequestMapping
 public class HelloController {
     private final HelloService helloService;
+    private final ApplicationContext applicationContext;
 
     // spring container looks up beans if there's a class that implements HelloService and injects it.
-    public HelloController(HelloService helloService) {
+    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
         this.helloService = helloService;
+        this.applicationContext = applicationContext;
     }
 
-    @GetMapping
+    @GetMapping("/hello")
 //    @ResponseBody // string return하게 되면 dispatcher servlet이 해당 이름에 맞는 view를 찾으려고 하기 때문에 해당 annotation을 달아서 body에 넣어준다.
 //    @RequestMapping(value = "/hello", method = RequestMethod.GET) // 윗줄과 같다.
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
+
+//    @Override
+//    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+//        System.out.println(applicationContext);
+//        this.applicationContext = applicationContext;
+//    }
 }

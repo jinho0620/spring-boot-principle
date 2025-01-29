79e5272173351b8dd6c8193a9c7a39ec6ce56c13
jinho0620 <jinhojo0620@gmail.com>
2025-01-29 14:05:43 +0900

    [Modify] add @Value annotation and PropertySourcesPlaceholderConfigurer to read property value

--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -1,5 +1,6 @@
 package tobyspring.config.autoconfig;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -13,13 +14,17 @@ import tobyspring.config.MyAutoConfiguration;
 @MyAutoConfiguration
 @ConditionalOnMyClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
+    @Value("${contextPath}")
+    String contextPath;
+
     // bean의 이름은 기본적으로 factory method 이름을 따라간다.
     // 따라서 같은 이름의 factory method가 있으면 충돌이 날 수 있기 때문에 아래와 같이 이름을 수정할 수 있다.
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean // 이미 등록된 bean이 있으면 등록 X
-    public ServletWebServerFactory servletWebServerFactory(Environment env) {
+    public ServletWebServerFactory servletWebServerFactory() {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setContextPath(env.getProperty("contextPath"));
+        System.out.println(this.contextPath);
+        factory.setContextPath(this.contextPath);
         return factory;
     }
 

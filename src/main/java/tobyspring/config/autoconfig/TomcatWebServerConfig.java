4d088b8c882ac5e9f4657a21e8a2cdd6ecbd56b4
jinho0620 <jinhojo0620@gmail.com>
2025-01-29 13:54:45 +0900

    [Test] read property value from .properties, environment variable and system property

--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.util.ClassUtils;
 import tobyspring.config.ConditionalOnMyClass;
@@ -16,9 +17,11 @@ public class TomcatWebServerConfig {
     // 따라서 같은 이름의 factory method가 있으면 충돌이 날 수 있기 때문에 아래와 같이 이름을 수정할 수 있다.
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean // 이미 등록된 bean이 있으면 등록 X
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+    public ServletWebServerFactory servletWebServerFactory(Environment env) {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.setContextPath(env.getProperty("contextPath"));
+        return factory;
     }
 
-    
+
 }
--- src/main/java/tobyspring/helloboot/HelloBootApplication.java
+++ src/main/java/tobyspring/helloboot/HelloBootApplication.java
@@ -1,11 +1,23 @@
 package tobyspring.helloboot;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import tobyspring.config.MySpringBootApplication;
 
 
 @MySpringBootApplication
 public class HelloBootApplication {
+
+	@Bean
+	ApplicationRunner applicationRunner(Environment env) { // Environment : 환경 설정 정보를 추상화 해놓은 bean
+		// interface에 method가 1개만 있을 때는 lambda식으로 쓸 수 있다.
+		return args -> {
+			String name = env.getProperty("my.name");
+			System.out.println("my.name: " + name);
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloBootApplication.class);
